2e4e29a27f86bd78482950a0628c1ccf77cecbbc
D.Taborda <101264990+darklley@users.noreply.github.com>
2023-12-28 11:25:57 -0500

    Alpha statistical.R

--- Alpha statistical.R
+++ Alpha statistical.R
@@ -8,10 +8,10 @@ library(ape)
 library(clhs)
 library(tidyverse)
 # Import data
-Tesis <- read_excel( "Datos.xlsx")
-Tesis$trt = as.factor(Tesis$trt)
-Tesis$blq = as.factor(Tesis$blq)
-Tesis$.id = as.factor(Tesis$.id)
+Data1 <- read_excel("Data.xlsx")
+Data1$trt = as.factor(Data1$trt)
+Data1$blq = as.factor(Data1$blq)
+Data1$.id = as.factor(Data1$.id)
 zm=read_rds("Z_matrix.rds")
 xm=read_rds("X_matrix.rds")
 alp=read_rds("alfas.rds")
@@ -19,16 +19,16 @@ alp=read_rds("alfas.rds")
 # Anova of the simulations and extraction of p and f values
 
 data = NULL
-for (i in 1:length(unique(Tesis$.id))) {
+for (i in 1:length(unique(Data1$.id))) {
   for (j in 7:1006) {
-    datos = Tesis |> 
-      dplyr::filter(.id == unique(Tesis$.id)[i])
+    datos = Data1 |> 
+      dplyr::filter(.id == unique(Data1$.id)[i])
     resaov = aov(data = datos,formula = unlist(datos[,j]) ~ blq + trt)
     fvt = summary(resaov)[[1]][4][2,1]
     fvb = summary(resaov)[[1]][4][1,1]
     pvt = summary(resaov)[[1]][5][2,1]
     pvb = summary(resaov)[[1]][5][1,1]
-    datax = data.frame(id = unique(Tesis$.id)[i], F_vT = fvt , 
+    datax = data.frame(id = unique(Data1$.id)[i], F_vT = fvt , 
                        F_vB = fvb,P_vT = pvt , P_vT = pvb ,  sim = paste0("sim",j))
     data = rbind.data.frame(data,datax)
   }
@@ -36,9 +36,9 @@ for (i in 1:length(unique(Tesis$.id))) {
 
 # Generate matrix
 
-generar_matrices <- function(datos, n) {
+generate_matrix <- function(datos, n) {
   num_filas <- nrow(datos)
-  matrices <- vector("list")
+matrixs <- vector("list")
   num_matrices <- ceiling(num_filas / n)
   for (i in 1:num_matrices) {
     inicio <- (i - 1) * n + 1
@@ -48,21 +48,21 @@ generar_matrices <- function(datos, n) {
     matriz  = 1/matriz
     diag(matriz) <- 0
     matriz = matriz/sum(matriz)
-    matrices[[i]] <- matriz
+  matrixs[[i]] <- matriz
   }
-  matrices
+matrixs
 }
-matrices_generadas <- generar_matrices(Tesis, 60)
+matrix_generate <- generate_matrix(Data1, 60)
 
 data2 = NULL
-for (i in 1:length(unique(Tesis$.id))) {
+for (i in 1:length(unique(Data1$.id))) {
   for (j in 1:1000) {
-    datos2 = Tesis |> 
-      dplyr::filter(.id == unique(Tesis$.id)[i])
+    datos2 = Data1 |> 
+      dplyr::filter(.id == unique(Data1$.id)[i])
     dat = datos2[,-c(1:6,1007)]
     IM =  Moran.I(dat[[j]], 
-                  matrices_generadas[[i]])$p.value
-    datax2 = data.frame(id = unique(Tesis$.id)[i], 
+                  matrix_generate[[i]])$p.value
+    datax2 = data.frame(id = unique(Data1$.id)[i], 
                         IMorant =  IM, sim = paste0("sim",j))
     data2 = rbind.data.frame(data2,datax2)
   }
@@ -74,18 +74,19 @@ X0= xm %*% zm
 Mo=  X0%*%solve(t(X0)%*%X0)%*%t(X0)
 IDEN = matrix(nrow = 60,ncol = 60,data = rep(0,60))
 diag(IDEN)<-1
-apha <- (t(Tesis$sim781[1:60]) %*% Mo %*% matrices_generadas[[1]] %*% (IDEN - Mo) %*% Tesis$sim781[1:60]) /
-  (t(Tesis$sim781[1:60]) %*% Mo %*% matrices_generadas[[1]] %*% matrices_generadas[[1]] %*% Mo %*% Tesis$sim781[1:60])
+apha <- (t(Data1$sim781[1:60]) %*% Mo %*% matrix_generate[[1]] %*% (IDEN - Mo) %*% Data1$sim781[1:60]) /
+  (t(Data1$sim781[1:60]) %*% Mo %*% matrix_generate[[1]] %*% matrix_generate[[1]] %*% Mo %*% Data1$sim781[1:60])
 data3 <- NULL
 
-for (i in 1:length(unique(Tesis$.id))) {
+for (i in 1:length(unique(Data1$.id))) {
   for (j in 1:1000) {
-    datos3 <- Tesis %>%
-      dplyr::filter(.id == unique(Tesis$.id)[i])
+    datos3 <- Data1 %>%
+      dplyr::filter(.id == unique(Data1$.id)[i])
     dat3 <- datos3[,-c(1:6,1007)]
-    apha <- t(dat3[[j]]) %*% Mo %*% matrices_generadas[[i]] %*% (IDEN - Mo) %*% dat3[[j]] /
-      t(dat3[[j]]) %*% Mo %*% matrices_generadas[[i]] %*% matrices_generadas[[i]] %*% Mo %*% dat3[[j]]
-    datax3 <- data.frame(id = unique(Tesis$.id)[i], a = apha, sim = paste0("sim",j))
+    apha <- t(dat3[[j]]) %*% Mo %*% matrix_generate[[i]] %*% (IDEN - Mo) %*% dat3[[j]] /
+      t(dat3[[j]]) %*% Mo %*% matrix_generate[[i]] %*% matrix_generate[[i]] %*% Mo %*% dat3[[j]]
+    datax3 <- data.frame(id = unique(Data1$.id)[i], a = apha, sim = paste0("sim",j))
     data3 <- rbind.data.frame(data3, datax3)
   }
 }
+
